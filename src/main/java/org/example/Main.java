6b0572494ea6046aeff121a9f228b866546e97e2
Rafael Ch <rafael.cherman0@gmail.com>
2023-07-28 19:11:06 -0300

    finalizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,72 +6,90 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*List<String> nomes = new ArrayList<>();
-        nomes.add("Lucas");
-        System.out.println(nomes);
 
+        List<Pessoa> pessoas = new ArrayList<>();
+        Pessoa pessoa = new Pessoa();
 
         Scanner ler = new Scanner(System.in);
-        String nome;
-
-        for(int i=0;i<3;i++)
+        int opc = 1;
+        while(opc != 3)
         {
-            nome = ler.next();
-            nomes.add(nome);
+            System.out.println("1 - Cadastrar uma pessoa");
+            System.out.println("2 - Buscar uma pessoa");
+            System.out.println("3 - Sair");
+            opc = ler.nextInt();
+            switch (opc){
+                case 1:
+                    pessoa = cadastrar();
+                    pessoas.add(pessoa);
+                    break;
+
+                case 2:
+                    exibir(pessoas);
+
+                case 3:
+                    break;
+
+                default:
+                    System.out.println("Opção invalida1");
+            }
         }
-        ler.close();
 
-        System.out.println(nomes);
-        System.out.println(nomes.size());
 
-        Pessoa lucas = new Pessoa("Lucas",19);
+    }
 
-        List<Pessoa> pessoas = new ArrayList<>();
-
-        pessoas.add(lucas);
-
-        System.out.println(pessoas.get(0).getNome());
-
-        pessoas.add(new Pessoa("Gabriel", 26));*/
-
-        List<Pessoa> pessoas = new ArrayList<>();
+    public static Pessoa cadastrar(){
 
         Scanner ler = new Scanner(System.in);
         String nome;
         String rua;
         int idade;
         int numero;
-        int opc;
-        for(int i=0;i<3;i++)
+        int opc = 1;
+
+        List<Endereco> enderecos = new ArrayList<>();
+        System.out.println("Digite o nome");
+        nome = ler.next();
+        System.out.println("Digite a idade");
+        idade = ler.nextInt();
+        while(opc != 2)
         {
-            List<Endereco> enderecos = new ArrayList<>();
-            opc = 1;
-            System.out.println("Digite o nome");
-            nome = ler.next();
-            System.out.println("Digite a idade");
-            idade = ler.nextInt();
-            while(opc == 1)
-            {
-                System.out.println("Digite a rua do endereço");
-                rua = ler.next();
-                System.out.println("Digite o numero do endereço");
-                numero = ler.nextInt();
-                enderecos.add(new Endereco(numero,rua));
-                System.out.println("Adicionar mais um endereço? 1-Sim  2-Não");
-                opc = ler.nextInt();
-            }
-            pessoas.add(new Pessoa(nome,idade,enderecos));
-
-
+            System.out.println("Digite a rua do endereço");
+            rua = ler.next();
+            System.out.println("Digite o numero do endereço");
+            numero = ler.nextInt();
+            enderecos.add(new Endereco(numero,rua));
+            System.out.println("Adicionar mais um endereço? 1-Sim  2-Não");
+            opc = ler.nextInt();
         }
+
+
+        return new Pessoa(nome,idade,enderecos);
+    }
+
+    public static void exibir(List<Pessoa> pessoas)
+    {
+        String nome;
+        boolean achou = false;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um nome");
+        nome = ler.next();
 
         for(Pessoa i : pessoas)
         {
-            System.out.println("Nome: " + i.getNome() + " - Idade: " + i.getIdade());
-            for(Endereco j : i.getEnderecos())
+            if(i.getNome().equals(nome))
             {
-                System.out.println("Numero: " + j.getNumero() + " - Rua: " + j.getRua());
+                System.out.println("Nome: " + i.getNome() + " - Idade: " + i.getIdade());
+                for (Endereco j : i.getEnderecos()) {
+                    System.out.println("Numero: " + j.getNumero() + " - Rua: " + j.getRua());
+                }
+                achou = true;
+                break;
             }
+        }
+        if(!achou)
+        {
+            System.out.println("Nome não encontrado");
         }
     }
 }
